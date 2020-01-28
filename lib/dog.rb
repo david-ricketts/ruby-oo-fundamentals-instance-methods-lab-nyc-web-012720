88db0ce2d704bcb6382675ece7880b5820a3007e
David Ricketts <david.ricketts119@gmail.com>
2020-01-28 22:47:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,5 +4,15 @@ class Dog
   def bark
     puts "Woof"
   end
-  
+  def sit
+    puts "The Dog is sitting"
+  end
 end
+
+class Person
+  def talk
+    puts "Hello World!"
+  end
+  def walk
+    puts "The Person is walking"
+  end 
